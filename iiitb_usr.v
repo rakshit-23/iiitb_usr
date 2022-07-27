8a4afa122fafca5859880aaa8236139475b1b16b
rakshit-23 <110079890+rakshit-23@users.noreply.github.com>
2022-07-27 18:08:56 +0530

    Update iiitb_usr.v

--- iiitb_usr.v
+++ iiitb_usr.v
@@ -1,21 +1,21 @@
-module iiitb_usr #(parameter MSB=8) ( data_in,data_out,clock,clear,select
+module iiitb_usr  (data_in,data_out,clock,clear,select,sl_ser,sr_ser
 
     );
-    input [MSB-1:0] data_in;
-    output reg [MSB-1:0] data_out;
-    input clear,clock;
+    input [7:0] data_in;
+    output reg [7:0] data_out;
+    input clear,clock,sl_ser,sr_ser;
     input [1:0] select;
     
     always@(posedge clock)
     begin
         if(clear==1'b1)
-            data_out<={MSB{1'b0}};
+            data_out<=8'b00000000;
         else
         begin 
-            if(select==2'b00)
-                data_out<={data_out[MSB-2:0],data_in[0]};
+            if(select==2'b00)   //Shift left
+                data_out<={data_out[6:0],sl_ser};
             else if(select==2'b01)
-                data_out<={data_in[7],data_out[MSB-1:1]};
+                data_out<={sr_ser,data_out[7:1]};
             else if(select==2'b10)
                 data_out<=data_in;
             else
