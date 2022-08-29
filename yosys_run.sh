8062d807857d66bce5b35f932e18665e276031eb
rakshit-23 <110079890+rakshit-23@users.noreply.github.com>
2022-08-29 14:39:04 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,8 +1,11 @@
+read_liberty -lib /home/rakshit/iiitb_usr/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 read_verilog iiitb_usr.v
 synth -top iiitb_usr
 dfflibmap -liberty /home/rakshit/iiitb_usr/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/rakshit/iiitb_usr/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/rakshit/iiitb_usr/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
-write_verilog iiitb_usr_synth.v
+write_verilog -noattr iiitb_usr_synth.v
+stat 
+show
